dfc324984ae58cd805f0a82fd315470e1e7e3c89
Shahriar Eshraghi <shahriareshraghi22@Gmail.com>
2022-06-19 13:13:10 +0430

    feat(i18n):install package

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'fa',
+      fallbackLocale: 'en',
+      localeDir: 'plugins/i18n/locales',
+      enableInSFC: false,
+      enableBridge: false,
+    },
+  },
+};
